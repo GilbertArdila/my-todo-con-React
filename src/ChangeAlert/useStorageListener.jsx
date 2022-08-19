71e4650b59bcb4d827bd9437326287b775ae3473
Gilbert Ardila <gilbertferney@gmail.com>
2022-08-19 11:32:02 -0500

    agregamos el archivo useLocalStorage.jsx lo habiamos olvidado

---
+++ src/ChangeAlert/useStorageListener.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+function useStorageListener
+    (sincronize) {
+
+    //creamos un estado para los cambios
+    const [storageChange, setStorageChange] = React.useState(false)
+    //llamamos a window para oir los cambios en el localStorage que estamos usando
+    window.addEventListener('storage', (change) => {
+        if (change.key === 'TODOS_V1') {
+            setStorageChange(true)
+        }
+    })
+    
+    const toggleShow = () => {
+        sincronize()
+        setStorageChange(false)
+    }
+    return { 
+        show:storageChange,
+        toggleShow
+    }
+
+}
+export {
+    useStorageListener
+}
